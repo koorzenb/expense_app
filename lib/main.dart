940d69835d9003b20ad7de2d4117da23f65f4e87
Barend Koorzen <koorzenb@gmail.com>
2022-09-14 05:38:31 +0200

    Formatting date

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -54,7 +55,7 @@ class MyHomePage extends StatelessWidget {
                   children: [
                     Container(
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -84,7 +85,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat.yMMMd().format(tx.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
